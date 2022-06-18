37469af32dab1bea3a0fb3fcd2214a40398d7c50
BarGreen <satanarchist69@gmail.com>
2022-06-18 21:57:26 +0300

    -- add configs and base structure: ini, locators, conftest

--- Tests/tc1_blouse_buying.py
+++ Tests/tc1_blouse_buying.py
@@ -87,7 +87,152 @@ try:
     # Находим кнопку создания аккаунта по id = SubmitCreate.
     button_submit_create = browser.find_element_by_id("SubmitCreate")
     button_submit_create.click()
-    time.sleep(9)
+
+    # Проверяем условие перехода на новую страницу. Ищем элемент с id = account-creation_form.
+    condition = WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.ID, "account-creation_form"))
+    )
+    
+    # 9. Заполняем форму регистрации.
+    # Находим radiobutton с выбором мужского пола при помощи id = id_gender1.
+    gender_radiobutton = browser.find_element_by_id("id_gender1")
+    gender_radiobutton.click()
+
+    # Находим input с вводом имени при помощи id = customer_firstname.
+    input_customer_firstname = browser.find_element_by_id("customer_firstname")
+    input_customer_firstname.send_keys("Semen")
+
+    # Находим input с вводом фамилии при помощи id = customer_lastname.
+    input_customer_lastname = browser.find_element_by_id("customer_lastname")
+    input_customer_lastname.send_keys("Yakovlev")
+
+    # Находим input с вводом email при помощи id = email.
+    # input_email_2 = browser.find_element_by_id("email")
+    # input_email_2.send_keys("mailyakov@yahoo.com")
+
+    # Находим input с вводом пароля при помощи id = passwd.
+    input_password = browser.find_element_by_id("passwd")
+    input_password.send_keys("SemenYakovleff1102")
+
+    # Инициализируем селект(выпадающий список с выбором) даты рождения по id = days/months/years.
+    select_day = Select(browser.find_element_by_id("days"))
+    select_month = Select(browser.find_element_by_id("months"))
+    select_year = Select(browser.find_element_by_id("years"))
+    # Выбираем 17 число через атрибут величины. Выбираем февраль (value=2). Выбираем 1999 год.
+    select_day.select_by_value("17")
+    select_month.select_by_value("2")
+    select_year.select_by_value("1999")
+
+    # Ставим галочки в чекбоксах на получение новостных рассылок.
+    # Находим первый checkbox с при помощи id = newsletter.
+    newsletter_checkbox = browser.find_element_by_id("newsletter")
+    newsletter_checkbox.click()
+    # Находим первый checkbox с при помощи id = optin.
+    optin_checkbox = browser.find_element_by_id("optin")
+    optin_checkbox.click()
+
+    # 10. Заполняем почтовую информацию.
+    # Находим input с вводом имени при помощи id = firstname.
+    input_firstname = browser.find_element_by_id("firstname")
+    input_firstname.send_keys("Semen")
+
+    # Находим input с вводом фамилии при помощи id = lastname.
+    input_lastname = browser.find_element_by_id("lastname")
+    input_lastname.send_keys("Yakovlev")
+
+    # Находим input с вводом компании при помощи id = company.
+    input_company = browser.find_element_by_id("company")
+    input_company.send_keys("MyCompany")
+
+    # Находим input с вводом адреса при помощи id = address1/2.
+    input_adress1 = browser.find_element_by_id("address1")
+    input_adress1.send_keys("Vvedensky st 3/2")
+    input_adress2 = browser.find_element_by_id("address2")
+    input_adress2.send_keys("450")
+
+    # Находим input с вводом города при помощи id = city.
+    input_city = browser.find_element_by_id("city")
+    input_city.send_keys("Moscow")
+
+    # Инициализируем селект(выпадающий список с выбором) штата по id = id_state.
+    select_state = Select(browser.find_element_by_id("id_state"))
+    # Выбираем Калифорнию под номером 5.
+    select_state.select_by_value("5")
+
+    # Находим input с вводом почтового кода при помощи id = postcode.
+    input_postcode = browser.find_element_by_id("postcode")
+    input_postcode.send_keys("00000")
+
+    # Находим textarea с вводом прочей информации при помощи id = other.
+    input_other = browser.find_element_by_id("other")
+    input_other.send_keys("I'm Semen Yakovlev from California.")
+
+    # Находим input с вводом домашнего телефона при помощи id = phone.
+    input_phone = browser.find_element_by_id("phone")
+    input_phone.send_keys("+7-925-221-50-08")
+
+    # Находим input с вводом мобильного телефона при помощи id = mobile_phone.
+    input_mphone = browser.find_element_by_id("mobile_phone")
+    input_mphone.send_keys("+7-915-411-22-15")
+
+    # Находим кнопку Submit по id = submitAccount.
+    button_account = browser.find_element_by_id("submitAccount")
+    button_account.click()
+
+    # Проверяем условие перехода на новую страницу. Ищем кнопку с name = processAddress.
+    condition = WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.NAME, "processAddress"))
+    )
+
+    # 11. Подтверждаем адрес.
+    # Находим кнопку по name = processAddress.
+    button_processAddress = browser.find_element_by_name("processAddress")
+    button_processAddress.click()
+
+    # Проверяем условие перехода на новую страницу. Ищем кнопку с name = processCarrier.
+    condition = WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.NAME, "processCarrier"))
+    )
+
+    # 12. Подтверждаем способ доставку.
+    # Находим кнопку по id = processCarrier.
+    button_processCarrier = browser.find_element_by_name("processCarrier")
+    button_processCarrier.click()
+
+    # Находим первый checkbox с при помощи id = cgv.
+    cgv_checkbox = browser.find_element_by_id("cgv")
+    cgv_checkbox.click()
+
+    # Проверяем условие перехода на новую страницу. Ищем ссылку на оплату по class = bankwire.
+    condition = WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.CLASS_NAME, "bankwire"))
+    )
+
+    # 13. Выбираем способ оплаты.
+    # Ищем ссылку на оплату по карте (class = bankwire).
+    a_bankwire = browser.find_element_by_class_name("bankwire")
+    a_bankwire.click()
+
+    # Проверяем условие перехода на новую страницу. Ищем ссылку по id = cart_navigation.
+    condition = WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.ID, "cart_navigation"))
+    )
+
+    # 14. Постверждаем заказ.
+    # Ищем кнопку по селекторам.
+    button_confirm = browser.find_element_by_css_selector(".button.btn.btn-default.standard-checkout.button-medium")
+    button_confirm.click()
+
+    # Проверяем условие перехода на новую страницу. Ищем ссылку по XPATH.
+    final_condition = WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.XPATH, "//strong[text()='Your order on My Store is complete.']"))
+    )
+
+    if final_condition:
+        print("TEST IS SUCCESSFUL")
+    else:
+        print("TEST FAILED")
+
 
     
 finally:
---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose lang")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption('language')
+    print("\nstart browser for test..")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = None
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
